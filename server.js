7282b99f1d2c04fcfb7a14f92a454ba88ad8c48d
Halvor Fladsrud Bø <halvorfb@stud.ntnu.no>
2019-01-08 11:52:44 +0100

    added env

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 
+console.log(process.env);
+
 const express = require('express');
 const fetch = require('node-fetch');
 
 const app = express();
 
 app.get('/', async (req, res) => {
-    console.log(process.env);
     try {
         const response = await fetch('http://dummy-service.svc.cluster.local:8080/');
         console.log(response);

4415b262e5ada54942d43b451c7e28ea5e73056f
Halvor Fladsrud Bø <halvorfb@stud.ntnu.no>
2019-01-08 11:45:08 +0100

    added try catch

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const fetch = require('node-fetch');
 const app = express();
 
 app.get('/', async (req, res) => {
-    const response = await fetch('http://dummy-service.svc.cluster.local:8080/');
-    console.log(response);
+    try {
+        const response = await fetch('http://dummy-service.svc.cluster.local:8080/');
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    } 
     await res.send('Hello world!');
 });
 

ea8682e4673218ef8013f8175200025ca836853c
Halvor Fladsrud Bø <halvorfb@stud.ntnu.no>
2019-01-08 11:40:41 +0100

    added fetch

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 
 const express = require('express');
+const fetch = require('node-fetch');
 
 const app = express();
 
 app.get('/', async (req, res) => {
+    const response = await fetch('http://dummy-service.svc.cluster.local:8080/');
+    console.log(response);
     await res.send('Hello world!');
 });
 
